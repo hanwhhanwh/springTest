ab0a4f979929610e6aba741e1ee492b32d42e9c8
hbesthee <hbesthee@naver.com>
2019-03-24 21:20:19 +0900

    add file_download example

--- src/ch15/UploadFile.java
+++ src/ch15/UploadFile.java
@@ -2,6 +2,7 @@ package ch15;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -37,13 +38,17 @@ public class UploadFile extends HttpServlet {
         System.out.println("UploadFile : doPost() 호출됨");
 
         request.setCharacterEncoding("utf-8");
+        response.setContentType("text/html; charset=utf-8");
 		String encoding = "utf-8";
 		File strUploadDir = new File("C:\\temp");
 		DiskFileItemFactory factory = new DiskFileItemFactory();
 		factory.setRepository(strUploadDir);
 		factory.setSizeThreshold(1024 * 1024); // Max file size ; 1MB
+		PrintWriter out = response.getWriter();
 
 		ServletFileUpload upload = new ServletFileUpload(factory);
+		out.println("<h1>파일 업로드 완료 / 다운로드 시험</h1>");
+		out.println("파일 다운로드 요청<br /><hr>");
 		try {
 			List<FileItem> fileItems = upload.parseRequest(request);
 //			for (int i = 0; i < fileItems.size(); i++) {
@@ -56,7 +61,9 @@ public class UploadFile extends HttpServlet {
 					System.out.println("파라미터명 : " + fileItem.getFieldName());
 					System.out.println("파일명 : " + fileItem.getName());
 					System.out.println("파일크기 : " + fileItem.getSize() + "bytes");
-			
+
+					out.println("<img src=\"/myShop/download.do?fileName=" + fileItem.getName() + "\" /><br />");
+					out.println("<a href=\"/myShop/download.do?fileName=" + fileItem.getName() + "\">이미지 다운로드</a><hr />");
 					if (fileItem.getSize() > 0) {
 						int idx = fileItem.getName().lastIndexOf("\\");
 						if (idx == -1) {
