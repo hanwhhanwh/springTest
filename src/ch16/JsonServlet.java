bc53031ed05ec472a4edbcec10c66f01ba355d88
hanwhhanwh <hanwhhanwh@gmail.com>
2019-03-29 00:46:00 +0900

    add json example

---
+++ src/ch16/JsonServlet.java
@@ -0,0 +1,117 @@
+package ch16;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+
+/**
+ * Servlet implementation class JsonServlet
+ */
+@WebServlet("/json")
+public class JsonServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public JsonServlet() {
+        super();
+        System.out.println("JsonServlet 생성됨");
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("JsonServlet : doGet() 호출됨");
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		String jsonInfo = request.getParameter("jsonInfo");
+		try {
+			JSONParser jsonParser = new JSONParser();
+			JSONObject jsonObject = (JSONObject) jsonParser.parse(jsonInfo);
+			System.out.println("* 회원 정보*");
+			System.out.println(jsonObject.get("name"));
+			System.out.println(jsonObject.get("age"));
+			System.out.println(jsonObject.get("gender"));
+			System.out.println(jsonObject.get("nickname"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("JsonServlet : doPost() 호출됨");
+
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter writer = response.getWriter();
+
+		JSONObject totaObject = new JSONObject();
+		JSONArray membersArray = new JSONArray();
+		JSONObject memberInfo = new JSONObject();
+		memberInfo.put("name", "박지성");
+		memberInfo.put("age", "25");
+		memberInfo.put("gender", "남자");
+		memberInfo.put("nickname", "날센돌이");
+		membersArray.add(memberInfo);
+
+		memberInfo = new JSONObject();
+		memberInfo.put("name", "김연아");
+		memberInfo.put("age", "21");
+		memberInfo.put("gender", "여자");
+		memberInfo.put("nickname", "칼치");
+		membersArray.add(memberInfo);
+		totaObject.put("members", membersArray);
+
+		JSONArray bookArray = new JSONArray();
+		JSONObject bookInfo = new JSONObject();
+		bookInfo.put("title", "초보자를 위한 자바 프로그래밍");
+		bookInfo.put("writer", "이병승");
+		bookInfo.put("price", "30000");
+		bookInfo.put("genre", "IT");
+		bookInfo.put("image", "http://localhost:8090/pro16/image/image1.jpg");
+		bookArray.add(bookInfo);
+
+		bookInfo = new JSONObject();
+		bookInfo.put("title", "모두의 파이썬");
+		bookInfo.put("writer", "이승찬");
+		bookInfo.put("price", "12000");
+		bookInfo.put("genre", "IT");
+		bookInfo.put("image", "http://localhost:8090/pro16/image/image2.jpg");
+		bookArray.add(bookInfo);
+
+		totaObject.put("books", bookArray);
+
+		String jsonInfo = totaObject.toJSONString();
+		System.out.print(jsonInfo);
+		writer.print(jsonInfo);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("JsonServlet : doPut() 호출됨");
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("JsonServlet : doDelete() 호출됨");
+	}
+
+}
