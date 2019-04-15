3e37db35d4c034a537bcfee9730f0adcd86b1333
hanwhhanwh <hanwhhanwh@gmail.com>
2019-04-16 00:32:10 +0900

    add first mybatis example

---
+++ src/ch23/MemberServlet1.java
@@ -0,0 +1,33 @@
+package ch23;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/mem1.exe")
+public class MemberServlet1 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	private void doHandle(HttpServletRequest request, HttpServletResponse response)	throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		MemberDAO dao = new MemberDAO();
+		List<MemberVO> membersList = dao.selectAllMemberList();
+		request.setAttribute("membersList", membersList);
+        RequestDispatcher dispatch = request.getRequestDispatcher("/ch23/listMembers1.jsp");
+		dispatch.forward(request, response);
+	}
+}
