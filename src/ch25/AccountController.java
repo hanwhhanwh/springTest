2a0fa8560d50e186d9afae8a7f5b2689c819e7db
hanwhhanwh <hanwhhanwh@gmail.com>
2019-04-21 13:17:35 +0900

    add spring transaction example

---
+++ src/ch25/AccountController.java
@@ -0,0 +1,21 @@
+package ch25;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+public class AccountController  extends MultiActionController  {
+	   private AccountService accService ; 
+	   public void setAccService(AccountService accService){
+	     this.accService = accService;
+	   }	
+
+	   public ModelAndView sendMoney(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	      ModelAndView mav=new ModelAndView();
+	      accService.sendMoney();
+	      mav.setViewName("result");
+	      return mav;
+	   }
+}
